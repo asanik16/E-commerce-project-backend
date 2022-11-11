148ec93b7b55a206ff4c564647c4565fad0189b2
Atik Shahriar Anik <asanik16@gmail.com>
2022-11-12 01:02:22 +0600

    added create update and delete functions from mongodb atlas

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const env = require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
+// const router = express.Router();
 const app = express();
 app.use(cors());
-const person = mongoose.model('product', {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json());
+const Person = mongoose.model('product', {
     productName: String,
     productCode: String,
     category: String,
@@ -13,19 +18,34 @@ const person = mongoose.model('product', {
     description: String,
     imageURL: String,
 });
-app.get('/api', (req, res) => {
-    person.find().then((result) => res.json(result));
-    // const rashed = person.create({
-    //     productName: 'goza rashed',
-    //     productCode: 'gr',
-    //     category: 'String',
-    //     price: 100,
-    //     description: 'String',
-    //     imageURL: 'String',
-    // });
-    // rashed.save();
-    // res.send('hello');
+app.get('/get-product', (req, res) => {
+    Person.find().then((result) => res.json(result));
+    // res.send(JSON.stringify(product));
 });
+
+app.post('/create-product', async (req, res) => {
+    console.log('receiving data');
+    console.log(req.body);
+    // eslint-disable-next-line no-new
+    const newperson = new Person(req.body);
+    const output = await newperson.save();
+    res.send(output);
+    // person.push(req.body);
+    // res.send(JSON.stringify(person));
+});
+
+app.put('/edit-product/:id', async (req, res) => {
+    const { id } = req.params;
+    const output = await Person.findByIdAndUpdate(id, req.body);
+    res.send(output);
+});
+
+app.delete('/delete-product/:id', async (req, res) => {
+    const { id } = req.params;
+    const output = await Person.findByIdAndDelete(id);
+    res.send(output);
+});
+
 const port = process.env.port || 3010;
 mongoose.connect(process.env.mongoURL, () => {
     console.log('database e aslam');
